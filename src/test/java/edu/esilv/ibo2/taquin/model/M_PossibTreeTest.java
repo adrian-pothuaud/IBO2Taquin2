6b9e66e52921463edb48ae4ac7f1e2720685dc6f
Adrian Pothuaud <adrianpothuaud2@gmail.com>
2017-03-09 18:18:35 +0100

    M_PossibTreeTests ok for 1-turn basic Tree building

--- src/test/java/edu/esilv/ibo2/taquin/model/M_PossibTreeTest.java
+++ src/test/java/edu/esilv/ibo2/taquin/model/M_PossibTreeTest.java
@@ -1,42 +1,124 @@
 package edu.esilv.ibo2.taquin.model;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class M_PossibTreeTest {
 
-    private M_PossibTree myTree;
-
-    @Before
-    public void setUp() throws Exception {
-        // PriorityQueue<M_PossibNode> myQueue = new PriorityQueue<M_PossibNode>();
-        M_Grid myGrid = new M_Grid();
-        myTree = new M_PossibTree(myGrid);
-    }
-
     @Test
     public void basicTree() throws Exception {
+
+        M_Grid myGrid = new M_Grid(new ArrayList<Integer>() {{
+            add(0);
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+            add(6);
+            add(7);
+            add(8);
+            add(9);
+            add(10);
+            add(11);
+            add(12);
+            add(13);
+            add(14);
+            add(15);
+        }});
+
+        M_PossibTree myTree = new M_PossibTree(myGrid);
         M_Grid rootgrid = myTree.getRoot().getGrid();
         M_Position actualZeroPos = rootgrid.getCaseByKey(0).getPos();
         M_Position posToMove;
 
         if(actualZeroPos.getLeft().isInGrid()) {
             posToMove = actualZeroPos.getLeft();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
         }
-        else if(actualZeroPos.getTop().isInGrid()) {
+        if(actualZeroPos.getTop().isInGrid()) {
             posToMove = actualZeroPos.getTop();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
         }
-        else if(actualZeroPos.getRight().isInGrid()) {
+        if(actualZeroPos.getRight().isInGrid()) {
             posToMove = actualZeroPos.getRight();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
         }
-        else if(actualZeroPos.getBottom().isInGrid()) {
+        if(actualZeroPos.getBottom().isInGrid()) {
             posToMove = actualZeroPos.getBottom();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
         }
         else {
             throw new Exception("Actual zero case has no movable neighbour !");
         }
 
-        M_Grid newGrid = new M_Grid(rootgrid);
-        newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+        assert myTree.getRoot().getNbChilds() == 2;
+    }
+
+    @Test
+    public void basicTree2() throws Exception {
+
+        M_Grid myGrid = new M_Grid(new ArrayList<Integer>() {{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+            add(6);
+            add(0);
+            add(7);
+            add(8);
+            add(9);
+            add(10);
+            add(11);
+            add(12);
+            add(13);
+            add(14);
+            add(15);
+        }});
+
+        M_PossibTree myTree = new M_PossibTree(myGrid);
+        M_Grid rootgrid = myTree.getRoot().getGrid();
+        M_Position actualZeroPos = rootgrid.getCaseByKey(0).getPos();
+        M_Position posToMove;
+
+        if(actualZeroPos.getLeft().isInGrid()) {
+            posToMove = actualZeroPos.getLeft();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
+        }
+        if(actualZeroPos.getTop().isInGrid()) {
+            posToMove = actualZeroPos.getTop();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
+        }
+        if(actualZeroPos.getRight().isInGrid()) {
+            posToMove = actualZeroPos.getRight();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
+        }
+        if(actualZeroPos.getBottom().isInGrid()) {
+            posToMove = actualZeroPos.getBottom();
+            M_Grid newGrid = new M_Grid(rootgrid);
+            newGrid.moveCase(rootgrid.getCaseByPos(posToMove));
+            myTree.getRoot().addChild(newGrid);
+        }
+        else {
+            throw new Exception("Actual zero case has no movable neighbour !");
+        }
+
+        assert myTree.getRoot().getNbChilds() == 4;
     }
 }
