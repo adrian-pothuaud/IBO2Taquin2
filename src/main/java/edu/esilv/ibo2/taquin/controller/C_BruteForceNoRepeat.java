6e32845bfa0f3e60476a926b36545d04c5d264ce
adrianpothuaud <adrianpothuaud2@gmail.com>
2017-03-13 14:25:25 +0100

    Graphics linked to Grid class

--- src/main/java/edu/esilv/ibo2/taquin/controller/C_BruteForceNoRepeat.java
+++ src/main/java/edu/esilv/ibo2/taquin/controller/C_BruteForceNoRepeat.java
@@ -4,6 +4,7 @@ import edu.esilv.ibo2.taquin.model.M_Grid;
 import edu.esilv.ibo2.taquin.model.M_PossibNode;
 import edu.esilv.ibo2.taquin.model.M_PossibTree;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 public class C_BruteForceNoRepeat {
@@ -14,14 +15,21 @@ public class C_BruteForceNoRepeat {
     private int cptPerfect;
 
     public void C_BruteForceNoRepeat() {
-        System.out.println("Building Tree for BrutoForceNoRepeat solver");
+        System.out.println("Building Tree for BruteForceNoRepeat solver");
         M_Grid g = new M_Grid();
         while (g.isSimilarTo(perfect)) g = new M_Grid();
         myTree = new M_PossibTree(g);
+        computed = new ArrayList<M_PossibNode>();
     }
 
-    public void build() {
-
+    public void buildL1() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        // build root childs
+        myTree.buildChilds(myTree.getRoot());
+        for (M_PossibNode child : myTree.getRoot().getChildrens()) {
+            if (!child.gridEquals(perfect)) {
+                computed.add(computed.size() - 1 , child);
+            }
+        }
     }
 
 }
